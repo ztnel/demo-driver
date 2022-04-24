af7b86662bf643c1b6d623b2b94d394c8dfac01e
ztnel <christian@leapsystems.online>
2022-04-24 03:07:58 +0100

    add some new descriptors

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 
-#include <linux/init.h>
-#include <linux/module.h>
+#include <linux/init.h>       /* module_init, module_exit */
+#include <linux/module.h>     /* version info, MODULE_LICENSE, MODULE_AUTHOR, printk() */
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Linux Test Driver");
+MODULE_AUTHOR("Christian Sargusingh")
 
 static int __init test_init(void) {
-  printk(KERN_INFO "Hello World.\n");
+  printk(KERN_NOTICE "Hello World.\n");
   return 0;
 }
 
 static void __exit test_exit(void) {
-  printk(KERN_ALERT "Goodbye World.\n");
+  printk(KERN_NOTICE "Goodbye World.\n");
   return;
 }
 
 module_init(test_init);
 module_exit(test_exit);
-MODULE_LICENSE("GPL");
